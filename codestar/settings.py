92510aabeeb9c53fb9edbbaba4a9e76ebf7c8f23
atm2chx3@students.codeinstitute.net <atm2chx3@students.codeinstitute.net>
2025-03-11 11:07:18 +0000

    Create a procfile and make changes to settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u8tv&j539g3x^3tj9567pkq7&*+n^_2i0s&f*&2at5!m-yd0ue'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
